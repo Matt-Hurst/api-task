7d8bffa35fbb0d54f1aa6131cd123d028a5527fb
Matt Hurst <matt.adam.hurst@gmail.com>
2021-02-06 09:53:21 +0000

    feat(index.ts): add cors to index.ts - set up trusted origins when provided

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 4000
+
 const app = express()
+
+//TODO: set up Stuart API and Dispatcher as trusted origins
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello from server')
